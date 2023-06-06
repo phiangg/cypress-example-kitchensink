29e7e1fb37a6e74605abd766edc933cca560aefc
Phiangg <sophiaquizon1102@gmail.com>
2023-06-06 16:02:21 +0800

    add todo item

--- cypress/e2e/cypress-todo-app.cy.js
+++ cypress/e2e/cypress-todo-app.cy.js
@@ -3,9 +3,16 @@ describe('template spec', () => {
     cy.visit('http://localhost:8080/todo')
   })
 
-  instanceof('can add a todo item', () => {
-    cy.get('input').type("Kill Akio")
-    cy.
+  it('can add a todo item', () => {
+  cy.get('[data-test="new-todo"]').type('Kill Akio {enter}')
+  cy.get('.todo-list li').eq(2).should('have.text', 'Kill Akio')
   })
+
+  it.only('can check "Walk the dog"', () => {
+  cy.get('li').contains('Walk the dog').find('input[type="checkbox"]').check()
+  cy.get('li').contains('Walk the dog').find('input[type="checkbox"]').should('be.checked')
+  })
+
+
 })
 

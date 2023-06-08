da58457f21a497132d0a4589696bbea3f097ecf1
phiangg <sophiaquizon1102@gmail.com>
2023-06-08 16:11:14 +0800

    5 tasks completed

--- cypress/e2e/cypress-todo-app.cy.js
+++ cypress/e2e/cypress-todo-app.cy.js
@@ -1,4 +1,4 @@
-describe('template spec', () => {
+describe('Kitchen Sink', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080/todo')
   })
@@ -8,11 +8,33 @@ describe('template spec', () => {
   cy.get('.todo-list li').eq(2).should('have.text', 'Kill Akio')
   })
 
-  it.only('can check "Walk the dog"', () => {
-  cy.get('li').contains('Walk the dog').find('input[type="checkbox"]').check()
-  cy.get('li').contains('Walk the dog').find('input[type="checkbox"]').should('be.checked')
+  it('can check "Walk the dog"', () => {
+    cy.contains('.todo-list li', 'Walk the dog')
+      .find('.toggle')
+      .check()
+      .should('be.checked');
+  });
+  
+  it('asserts that there are 3 items', () => {
+    cy.get('[data-test="new-todo"]').type('Kill Akio {enter}')
+    cy.get('.todo-list li').should('have.length', 3)
   })
 
+  it('can clear completed', () => {
+  cy.contains('.todo-list li', 'Walk the dog')
+    .find('input[type="checkbox"]')
+    .check()
+    .should('be.checked');
+
+  cy.contains('button', 'Clear completed').click();
+
+  cy.contains('.todo-list li', 'Walk the dog').should('not.exist');
+});
+  
+  it('can check all', () => {
+    cy.get('label[for="toggle-all"]').click()
+    cy.get('input[type="checkbox"]').should('be.checked')
+    })
 
 })
 
